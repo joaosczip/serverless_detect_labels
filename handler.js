89a7a75b5b10625939397cd9601e72fd132c7e08
João Guilherme <joao.sczip@hotmail.com>
2020-04-12 16:03:34 -0300

    uploading project

---
+++ handler.js
@@ -0,0 +1,104 @@
+"use strict";
+
+const axios = require("axios");
+
+class Handler {
+  constructor({ rekoSvc, translatorSvc, httpSvc }) {
+    this.rekoSvc = rekoSvc;
+    this.translatorSvc = translatorSvc;
+    this.httpSvc = httpSvc;
+  }
+
+  async detectImageLabels(buffer) {
+    const result = await this.rekoSvc
+      .detectLabels({
+        Image: {
+          Bytes: buffer,
+        },
+      })
+      .promise();
+
+    const workingItems = result.Labels.filter(
+      ({ Confidence }) => Confidence > 98
+    );
+    const names = workingItems.map(({ Name }) => Name).join(" and ");
+
+    return { names, workingItems };
+  }
+
+  async translateText(text) {
+    const params = {
+      SourceLanguageCode: "en",
+      TargetLanguageCode: "pt",
+      Text: text,
+    };
+
+    const { TranslatedText } = await this.translatorSvc
+      .translateText(params)
+      .promise();
+
+    return TranslatedText.split(" e ");
+  }
+
+  formatTextResults(texts, workingItems) {
+    const finalText = [];
+    for (const indexText in texts) {
+      const nameInPortuguese = texts[indexText];
+      const confidence = workingItems[indexText].Confidence;
+      finalText.push(
+        `${confidence.toFixed(2)} de ser do tipo ${nameInPortuguese}`
+      );
+    }
+    return finalText;
+  }
+
+  async getImageBuffer(imageUrl) {
+    const response = await this.httpSvc.get(imageUrl, {
+      responseType: "arraybuffer",
+    });
+
+    const buffer = Buffer.from(response.data);
+    return buffer;
+  }
+
+  async main(event) {
+    try {
+      const { imageUrl } = event.queryStringParameters;
+
+      console.log("Downloading image...");
+      const buffer = await this.getImageBuffer(imageUrl);
+
+      console.log("Detecting labels...");
+      const { names, workingItems } = await this.detectImageLabels(buffer);
+
+      console.log("Transaling to portuguese..");
+      const texts = await this.translateText(names);
+
+      console.log("Handling final object...");
+      const finalText = this.formatTextResults(texts, workingItems);
+
+      console.log("Finishing...");
+      return {
+        statusCode: 200,
+        body: `A imagem tem\n`.concat(finalText),
+      };
+    } catch (error) {
+      console.log("Error***", error.stack);
+      return {
+        statusCode: 500,
+        body: "Internal Server Error",
+      };
+    }
+  }
+}
+
+const aws = require("aws-sdk");
+const reko = new aws.Rekognition();
+const translator = new aws.Translate();
+
+const handler = new Handler({
+  rekoSvc: reko,
+  translatorSvc: translator,
+  httpSvc: axios,
+});
+module.exports.main = handler.main.bind(handler);
